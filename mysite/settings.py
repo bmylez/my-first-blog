d9897f5d76cdd5a1b5288c2dae092484996516a4
Brian Myles <bcmyles86@gmail.com>
2015-05-17 16:47:28 -0500

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,6 +100,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = 'C:\Users\Brian\Documents\Django_tutorial\staticfiles'
+STATIC_ROOT = 'C:/Users/Brian/Documents/Django_tutorial/staticfiles'
 
 STATIC_URL = '/static/'
